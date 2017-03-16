370403e7696ac77bb2e15bda79d371f974397789
Scott Savage <snsavage@gmail.com>
2017-03-16 11:33:17 -0400

    Pass all lab tests

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,52 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
 function switchAge(age) {
+  var teenager = "You are a teenager";
 
+  switch (age) {
+    case 13:
+      return teenager;
+    case 14:
+      return teenager;
+    case 15:
+      return teenager;
+    case 16:
+      return teenager;
+    case 17:
+      return teenager;
+    case 18:
+      return teenager;
+    case 19:
+      return teenager;
+    default:
+      return "You have an age";
+  }
 }
+
