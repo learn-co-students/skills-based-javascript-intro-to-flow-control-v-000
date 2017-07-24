e83b5d3523e7361c04fc7704935a0f87728c31be
mirandalily <mirandaraymond@me.com>
2017-07-24 13:04:43 -0700

    all tests passing

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "you are a teenager!"
+    return "You are a teenager!"
   } else if (age <= 12) {
     return "You are a kid"
   } else if (age >= 20) {
@@ -27,5 +27,16 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
+    default:
+      return "You have an age"
+  }
 }
