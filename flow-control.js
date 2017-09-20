f4d39d41d1a11b64cb1926779dc8e58182b71ef7
Pauli Payne <pauli.a.payne@gmail.com>
2017-09-20 20:11:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,30 +1,30 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
-  }
-}
+    return "You are a teenager!";
+  };
+};
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
-  }
-}
+    return "You are not a teenager";
+  };
+};
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else if (age <= 12){
-    return "You are a kid"
+    return "You are a kid";
   } else if (age >= 20) {
-    return "You are a grownup"
-  }
-}
+    return "You are a grownup";
+  };
+};
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
-}
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
+};
 
 function switchAge(age) {
   switch(age) {
@@ -52,5 +52,5 @@ function switchAge(age) {
     default:
       return "You have an age";
       break;
-  }
-}
+  };
+};
