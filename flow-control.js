72f9f7d7c445d2078b19d62c2650f5d48ffadff0
smithwebtek <brad@smithwebtek.com>
2017-09-21 14:31:50 -0400

    add switch and ternary examples

--- flow-control.js
+++ flow-control.js
@@ -33,9 +33,25 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  if (age >= 13 && age <= 19) { 
-    return "You are a teenager";
-  } else {
-    return "You have an age";
-  }
+  switch(age){
+    case 13:
+      return "You are a teenager"
+    case 14:
+      return "You are a teenager"
+    case 15:
+      return "You are a teenager"
+    case 16:
+      return "You are a teenager"
+    case 17:
+      return "You are a teenager"
+    case 18:
+      return "You are a teenager"
+    case 19:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+    }
+}
+function ternaryAge(age) {
+  (age >= 13 && age <= 19) ? console.log("You are a teenager!") : console.log("You are not a teenager");
 }
